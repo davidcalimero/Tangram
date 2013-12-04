5dfccd694088d07b1df6199d76b8ab4a99ea0b2c
Fábio David Da Silva Alves <fabio.alves@ist.utl.pt>
2013-12-04 22:15:20 +0000

    Mudança de Centro de Massa
    Mudança das translacções aos objectos, para ajustar aos centros de
    massa.

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -24,47 +24,49 @@ Entity * GameManager::getEntityById(std::string id){
 
 
 void GameManager::init(){
-
+	
+	/**/
 	TangramPieces * tabuleiro = new TangramPieces("tabuleiro", "tabuleiro.xml");
 	tabuleiro->scale(1.8,1.8,0.1);
-	tabuleiro->setPos(-0.9,-0.9,-0.1,0.0);
+	tabuleiro->setPos(0,0,-0.05,0);
 	add(tabuleiro);
-
+	/**/
 	TangramPieces * trianguloVermelho = new TangramPieces("trianguloVermelho", "trianguloVermelho.xml");
 	trianguloVermelho->scale(sqrt(2.0)/2.0,sqrt(2.0)/2.0,0.25);
-	trianguloVermelho->setPos(0.0,0.0,0.0,45.0);
+	trianguloVermelho->setPos(0.0,1/4.0,0.125,45.0);
 	add(trianguloVermelho);
-
+	/**/
 	TangramPieces * trianguloRoxo = new TangramPieces("trianguloRoxo", "trianguloRoxo.xml");
 	trianguloRoxo->scale(sqrt(2.0)/2.0,sqrt(2.0)/2.0,0.23);
-	trianguloRoxo->setPos(0.0,0.0,0.0,135.0);
+	trianguloRoxo->setPos(-1/4.0,0,0.115,135.0);
 	add(trianguloRoxo);
-
+	/**/
 	TangramPieces * trianguloAzul = new TangramPieces("trianguloAzul", "trianguloAzul.xml");
 	trianguloAzul->scale(1.0/2.0,1.0/2.0,0.13);
-	trianguloAzul->setPos(0.5,-0.5,0.0,90.0);
+	trianguloAzul->setPos(0.375,-0.375,0.065,90.0);
 	add(trianguloAzul);
-
+	/**/
 	TangramPieces * trianguloVerde = new TangramPieces("trianguloVerde", "trianguloVerde.xml");
 	trianguloVerde->scale(sqrt(2.0)/4.0,sqrt(2.0)/4.0,0.21);
-	trianguloVerde->setPos(-0.25,-0.25,0.0,-135.0);
+	trianguloVerde->setPos(-0.25, -0.375,0.105,-135.0);
 	add(trianguloVerde);
-
+	/**/
 	TangramPieces * trianguloRosa = new TangramPieces("trianguloRosa", "trianguloRosa.xml");
 	trianguloRosa->scale(sqrt(2.0)/4.0,sqrt(2.0)/4.0,0.17);
-	trianguloRosa->setPos(0.0,0.0,0.0,-45.0);
+	trianguloRosa->setPos(0.125,0.0,0.085,-45.0);
 	add(trianguloRosa);
-
+	/**/
 	TangramPieces * quadradoLaranja = new TangramPieces("quadradoLaranja", "quadradoLaranja.xml");
 	quadradoLaranja->scale(sqrt(2.0)/4.0,sqrt(2.0)/4.0,0.19);
-	quadradoLaranja->setPos(0.0,0.0,0.0,-135.0);
+	quadradoLaranja->setPos(0.0,-0.25,0.095,-135.0);
 	add(quadradoLaranja);
-
+	/**/
 	TangramPieces * quadradoAmarelo = new TangramPieces("quadradoAmarelo", "quadradoAmarelo.xml");
 	quadradoAmarelo->scale(1.0/4.0,1.0/2.0,0.15);
 	quadradoAmarelo->shear(0.0,1.0);
-	quadradoAmarelo->setPos(0.25,-0.25,0.0,0.0);
+	quadradoAmarelo->setPos(0.375,0.125,0.075,0.0);
 	add(quadradoAmarelo);
+	/**/
 }
 
 
