c3eeca441dd7bdb67070ee998cb4749fed716fad
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2013-12-19 10:33:08 +0000

    PostProcessing v3.0
    Shaders post-processing

--- scripts/shaders/fragmentPostProcessing.glsl
+++ scripts/shaders/fragmentPostProcessing.glsl
@@ -4,24 +4,13 @@
 in vec3 ex_Position;
 in vec4 ex_Color;
 in vec3 ex_Normal;
+in vec2 ex_TexCoord;
 
 // Out
 out vec4 out_Color;
 
-// Light Attributes
-uniform vec3 LightPosition;
-uniform vec3 AmbientLight;
-uniform vec3 DiffuseLight;
-uniform vec3 SpecularLight;
-
-// Material Attributes
-uniform vec3 MaterialAmbient;
-uniform vec3 MaterialDiffuse;
-uniform vec3 MaterialSpecular;
-uniform float MaterialShininess;
-
-// Camera View Vector
-uniform vec3 EyeDirection;
+// Texture Attributes
+uniform sampler2D tex;
 
 
 // Matrix
@@ -45,121 +34,109 @@ vec4 blend(vec4 src, vec4 dst){
 
 }
 
+
 /// 2D Noise by Ian McEwan, Ashima Arts.
-vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+	vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+	vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+	vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
+	
+	float snoise (vec2 v) {
+		const vec4 C = vec4( 0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+			                 0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+				            -0.577350269189626, // -1.0 + 2.0 * C.x
+					         0.024390243902439); // 1.0 / 41.0
 
-float snoise (vec2 v)
-{
-    const vec4 C = vec4( 0.211324865405187,  // (3.0-sqrt(3.0))/6.0
-                         0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
-                        -0.577350269189626, // -1.0 + 2.0 * C.x
-                         0.024390243902439); // 1.0 / 41.0
+		// First corner
+		vec2 i  = floor(v + dot(v, C.yy) );
+		vec2 x0 = v -   i + dot(i, C.xx);
 
-    // First corner
-    vec2 i  = floor(v + dot(v, C.yy) );
-    vec2 x0 = v -   i + dot(i, C.xx);
+		// Other corners
+		vec2 i1;
+		i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+		vec4 x12 = x0.xyxy + C.xxzz;
+		x12.xy -= i1;
 
-    // Other corners
-    vec2 i1;
-    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-    vec4 x12 = x0.xyxy + C.xxzz;
-    x12.xy -= i1;
+	   // Permutations
+		i = mod289(i); // Avoid truncation effects in permutation
+		vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 ));
 
-    // Permutations
-    i = mod289(i); // Avoid truncation effects in permutation
-    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-        + i.x + vec3(0.0, i1.x, 1.0 ));
+		vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+		m = m*m ;
+		m = m*m ;
 
-    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-    m = m*m ;
-    m = m*m ;
+		// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+		// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+		vec3 x = 2.0 * fract(p * C.www) - 1.0;
+		vec3 h = abs(x) - 0.5;
+		vec3 ox = floor(x + 0.5);
+		vec3 a0 = x - ox;
 
-    // Gradients: 41 points uniformly over a line, mapped onto a diamond.
-    // The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
-    vec3 x = 2.0 * fract(p * C.www) - 1.0;
-    vec3 h = abs(x) - 0.5;
-    vec3 ox = floor(x + 0.5);
-    vec3 a0 = x - ox;
+		// Normalise gradients implicitly by scaling m
+		// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+		m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
 
-    // Normalise gradients implicitly by scaling m
-    // Approximation of: m *= inversesqrt( a0*a0 + h*h );
-    m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+		// Compute final noise value at P
+		vec3 g;
+		g.x  = a0.x  * x0.x  + h.x  * x0.y;
+		g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+		return 130.0 * dot(m, g);
+}
 
-    // Compute final noise value at P
-    vec3 g;
-    g.x  = a0.x  * x0.x  + h.x  * x0.y;
-    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-    return 130.0 * dot(m, g);
+vec4 scratch(){
+	float xPeriod = 8.0;
+    float yPeriod = 1.0;
+    float pi = 3.141592;
+    float phase = 0.3;
+    float turbulence = snoise(ex_TexCoord * 2.5);
+    float vScratch = 0.5 + (sin(((ex_TexCoord.x * xPeriod + ex_TexCoord.y * yPeriod + turbulence)) * pi + phase) * 0.5);
+    vScratch = clamp((vScratch * 10000.0) + 0.35, 0.0, 1.0);
+	return vec4(vScratch, vScratch, vScratch, 1.0);
 }
 
 
 
-void main(void)
-{
+void main(void) {
+
+	vec4 text = texture(tex, ex_TexCoord);
 
 	// Sepia color
 	vec4 sepia_Color = vec4(112.0 / 255.0, 66.0 / 255.0, 20.0 / 255.0, 0.0);
 	vec4 final_Color;
-	float SepiaValue = 0.6;
-	float NoiseValue = 0.2;
+	float SepiaValue = 0.7;
+	float NoiseValue = 0.1;
 	float ScratchValue = 0.3;
-	float InnerVignetting = 0.3;
+	float InnerVignetting = 0.5;
 	float OuterVignetting = 1.0;
 	float RandomValue = 0.5;
 
-	// LIGHTS 
-		// Blinn-Phong Model
-		// Vector Initialization
-		vec3 L = normalize(vec3(ViewMatrix * vec4(LightPosition, 1.0)) - ex_Position); //sem view -> luz segue camara, com view -> luz segue cena
-		vec3 E = normalize(-EyeDirection);
-		vec3 N = normalize(ex_Normal);
-		vec3 H = normalize(L + E);
-
-		// Ambient Component
-		vec3 ambient = MaterialAmbient * AmbientLight;
-
-		// Diffuse Component
-		float NdotL = max(dot(N, L), 0.0);
-		vec3 diffuse = MaterialDiffuse * DiffuseLight * NdotL;
-
-		// Specular Component
-		vec3 specular = vec3(0.0);
-		if(NdotL > 0)
-		{
-			float NdotH = max(dot(N, H), 0.0);
-			specular = MaterialSpecular * SpecularLight * pow(NdotH, MaterialShininess);
-		}
-
-		vec4 light_Color = vec4(ambient + diffuse + specular, 1.0);
-
-
 	// SEPIA
 		// Grayscale Conversion
-		vec4 grayscale = grayscale(light_Color);
+		vec4 grayscale = grayscale(text);
+		final_Color = grayscale;
 
 		// Overload with sepia color
 		final_Color = blend(sepia_Color, grayscale);
 		final_Color = grayscale + SepiaValue * (sepia_Color - grayscale);
 	
-	// NOISE (eyeDirection = vUv)
-		float noise = snoise(
-					  	vec2( 800.0 + RandomValue * 600.0, 
-							800.0 + RandomValue * 600.0)) * 0.5;
-		final_Color = light_Color;
+
+	// NOISE
+		float noise = snoise(ex_TexCoord * vec2(800.0 + RandomValue * 600.0,  800.0 + RandomValue * 600.0)) * 0.5;
 		final_Color += noise * NoiseValue;
+
+		// Simulate ISO on camera
 		vec4 noise_Overlay = blend(final_Color, vec4(noise));
     	final_Color = final_Color + NoiseValue * (final_Color - noise_Overlay);
 	
 	// SCRATCHES
-
-  
+	float dist = (1.0 / ScratchValue);
+    float d = distance(ex_TexCoord, vec2(RandomValue * dist, RandomValue * dist));
+    final_Color *= scratch();
 
 	// VIGNETTING
-  	/*float d = distance(vec2(0.5, 0.5), vUv) * 1.414213;
-    float vignetting = clamp((OuterVignetting - d) / (OuterVignetting - InnerVignetting), 0.0, 1.0);
-    final_Color *= vignetting;*/
+  	float dist1 = distance(vec2(0.5, 0.5), ex_TexCoord) * 1.414213;
+    float vignetting = clamp((OuterVignetting - dist1) / (OuterVignetting - InnerVignetting), 0.0, 1.0);
+	final_Color *= vignetting;
 
+	
 	out_Color = final_Color;
 }
--- scripts/shaders/vertexPostProcessing.glsl
+++ scripts/shaders/vertexPostProcessing.glsl
@@ -4,12 +4,13 @@
 in vec3 in_Position;
 in vec4 in_Color;
 in vec3 in_Normal;
+in vec2 in_TexCoord;
 
 // Out
 out vec3 ex_Position;
 out vec4 ex_Color;
 out vec3 ex_Normal;
-
+out vec2 ex_TexCoord;
 
 // Matrix
 uniform mat4 ModelMatrix;
@@ -25,5 +26,6 @@ void main(void)
 	ex_Color = in_Color;
 	ex_Normal = normalize(NormalMatrix * in_Normal);
 	ex_Position = vec3(ViewMatrix * ModelMatrix * vec4(in_Position, 1.0));
+	ex_TexCoord = in_TexCoord;
 	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(in_Position, 1.0);
 }
